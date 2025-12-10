1981269b0df2860728af05aeda0f64430cc4d0c6
Ken-Cardon <ken.cardon@student.ap.be>
2025-12-10 21:47:49 +0100

    aanpassingen yml en setup

---
+++ ESP32_remote/src/main.cpp
@@ -0,0 +1,112 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+int led_pin = 0;
+int druk_pin = 4;
+
+bool isRunning = false;
+bool isForward = true;
+unsigned long startTime = 0;
+unsigned long elapsedTime = 0;
+
+const unsigned long MAX_DURATION = 10000;
+
+// variabelen WIFI
+const char WIFI_SSID[] = "Bletchley";
+const char WIFI_PASSWORD[] = "laptop!internet";
+
+// variabelen MQTT
+const char MQTT_HOST[] = "10.150.242.103";
+const int  MQTT_PORT = 1884;
+//const char MQTT_USER[] = "KenC";
+//const char MQTT_PASSWORD[] = "Kleintje7227!";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// MQTT publish
+void mqttSend(const char* topic, const String& payload) {
+  if (client.connected()) {
+    client.publish(topic, payload.c_str());
+  }
+}
+
+// Connect WiFi
+void connectWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+// Connect MQTT
+void connectMQTT() {
+  while (!client.connected()) {
+  client.connect("Garagepoort_pi");
+//client.connect("Garagepoort_pi", MQTT_USER, MQTT_PASSWORD);
+    delay(500);
+  }
+}
+
+void setup() {
+  pinMode(led_pin, OUTPUT);
+  pinMode(druk_pin, INPUT); // externe pulldown
+  digitalWrite(led_pin, LOW); // led uit bij start
+
+  WiFi.mode(WIFI_STA);
+  connectWiFi();
+
+  client.setServer(MQTT_HOST, MQTT_PORT);
+  connectMQTT();
+}
+
+void loop() {
+  client.loop();
+
+  bool pressed = digitalRead(druk_pin);
+
+  if (pressed) {
+    mqttSend("garage/button", "pressed");
+
+    if (!isRunning) {
+      // poort openen
+      isRunning = true;
+      startTime = millis();
+
+      elapsedTime = constrain(elapsedTime, 0, MAX_DURATION);
+      digitalWrite(led_pin, HIGH);
+
+      mqttSend("garage/state", isForward ? "opening" : "closing");
+      mqttSend("garage/progress", String(elapsedTime));
+    }
+    else {
+      // stoppen
+      isRunning = false;
+      elapsedTime = millis() - startTime;
+
+      digitalWrite(led_pin, LOW);
+
+      mqttSend("garage/state", "stopped");
+      mqttSend("garage/progress", String(elapsedTime));
+
+      isForward = !isForward;   // richting omkeren voor de volgende keer
+    }
+
+    delay(300); // debounce
+  }
+
+  if (isRunning) {
+    unsigned long currentElapsed = millis() - startTime;
+
+    mqttSend("garage/progress", String(currentElapsed));
+
+    if (currentElapsed >= MAX_DURATION) {
+      isRunning = false;
+      elapsedTime = 0;
+      digitalWrite(led_pin, LOW);
+
+      mqttSend("garage/state", isForward ? "open" : "closed");
+    }
+  }
+}
--- setup.sh
+++ setup.sh
@@ -9,20 +9,30 @@ fi
 
 apt update
 apt install -y mosquitto mosquitto-clients
+apt install -y apache2-utils
 
-mkdir -p influx_conf
+touch passwd
+# mkdir -p influx_conf
 
-echo "--- Mosquitto Beveiliging Setup ---"
-read -p "GEBRUIKERSNAAM?" mqtt_user
-if [ -z "$mqtt_user" ]; then
-    echo "Geen gebruikersnaam ingevoerd. Setup afgebroken."
-    exit 1
-fi
+#echo "--- Mosquitto Beveiliging Setup ---"
+#read -p "GEBRUIKERSNAAM?" mqtt_user
+#if [ -z "$mqtt_user" ]; then
+#    echo "Geen gebruikersnaam ingevoerd. Setup afgebroken."
+#    exit 1
+#fi
 
-echo "WACHTWOORD?"
-mosquitto_passwd -c passwd "$mqtt_user"
+#echo "WACHTWOORD?"
+#mosquitto_passwd -c passwd "$mqtt_user"
 
-chown mosquitto:mosquitto passwd
-chmod 600 passwd
+#chown mosquitto:mosquitto passwd
+#chmod 600 passwd
 
-echo "succes"
+#echo "succes"
+
+mkdir grafana_data
+chown 472:472 grafana_data
+mkdir node_red_data
+chown 1000:1000 node_red_data
+
+# read -p "Geef een user in voor toegang tot het node-red dashboard: " user
+# htpasswd passwd $user
